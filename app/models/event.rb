355312ec97fb1875ca1ad420418cafc93faa2605
github username <github email address>
2019-03-07 22:36:17 +0000

    tests-still-to-do

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ApplicationRecord
   has_many :users, through: :user_events
   belongs_to :location, optional: true
   belongs_to :category, optional: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates_uniqueness_of :name
 
   def display_start
     self.start_time.strftime("%B %e, %Y at %H:%M")
@@ -10,6 +13,14 @@ class Event < ApplicationRecord
 
   def display_end
     self.end_time.strftime("%B %e, %Y at %H:%M")
+  end
+
+  def self.active_events
+    Event.all.select {|event| event.end_time > DateTime.now}
+  end
+
+  def self.expired_events
+    Event.all.select {|event| event.end_time < DateTime.now}
   end
 
 end
